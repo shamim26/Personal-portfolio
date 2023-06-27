f540730b99532fafec425cac5494cce0349bf16b
shamim26 <shamimreja101@gmail.com>
2023-06-27 11:53:08 +0600

    nav added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return (
-    <div>
+    <div className="border-[1.7rem] z-10 border-custom2 bg-custom1 h-screen w-full text-white">
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import { Fade as Hamburger } from "hamburger-react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <div className="flex items-center justify-between relative px-14 py-8">
+      <h1 className="">Shamim</h1>
+      <div
+        className={`flex items-center lg:flex-row flex-col bg-custom1 w-full lg:w-auto left-0 absolute z-30 lg:static gap-20 lg:p-0 pt-10 lg:gap-10 transition-all ease-linear duration-300 ${
+          isOpen ? "top-20 h-screen" : "-top-[50rem] h-auto"
+        }`}
+      >
+        <NavLink to="#">Home</NavLink>
+        <NavLink to="#">About</NavLink>
+        <NavLink to="#">Projects</NavLink>
+        <NavLink to="#">Contact</NavLink>
+      </div>
+      <div className="lg:hidden">
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

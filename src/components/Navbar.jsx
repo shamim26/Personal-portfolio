79cb01745a9fcfb36a0987df29199c1629c95bb4
shamim26 <shamimreja101@gmail.com>
2023-06-27 13:41:40 +0600

    footer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
   return (
     <div className="flex items-center justify-between relative px-14 py-8">
-      <h1 className="">Shamim</h1>
+      <h1 className="uppercase text-3xl tracking-widest">Shamim</h1>
       <div
         className={`flex items-center lg:flex-row flex-col bg-custom1 w-full lg:w-auto left-0 absolute z-30 lg:static gap-20 lg:p-0 pt-10 lg:gap-10 transition-all ease-linear duration-300 ${
           isOpen ? "top-20 h-screen" : "-top-[50rem] h-auto"
